df6475e3661500969ec60db086feb9dbcca135ed
paulocfj <paulo.cfj2@gmail.com>
2020-08-11 15:20:40 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+//localhost:3333
+
+app.use(routes);
+
+app.listen(3333);
